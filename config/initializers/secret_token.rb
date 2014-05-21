36ab45dd2b3b10cbf2aefb9feaa6b7a120a6eae0
dudufangor <dudufangor@gmail.com>
2014-05-21 00:09:44 -0300

    eh nois

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CelticRo::Application.config.secret_token = '827e941279874ef7d7cfd4b9ca24248dfc06a412b484d0c23cef9f3e44fa1d49fca6c115500309bfa94e5f695a191d5b3bb9dd03a06041009280e10e51464c87'
