9904da3b81aeaa0e83e2cf40b11a9bb603ef9d17
dudufangor <dudufangor@gmail.com>
2014-05-22 01:43:46 -0300

    current

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,16 +1,16 @@
 # Set the working application directory
 # working_directory '/path/to/your/app'
-working_directory '/root/celtic_ro'
+working_directory '/root/celtic_ro/current'
 
 # Unicorn PID file location
 # pid '/path/to/pids/unicorn.pid'
-pid '/root/celtic_ro/pids/unicorn.pid'
+pid '/root/celtic_ro/current/pids/unicorn.pid'
 
 # Path to logs
 # stderr_path '/path/to/log/unicorn.log'
 # stdout_path '/path/to/log/unicorn.log'
-stderr_path '/root/celtic_ro/log/unicorn.log'
-stdout_path '/root/celtic_ro/log/unicorn.log'
+stderr_path '/root/celtic_ro/current/log/unicorn.log'
+stdout_path '/root/celtic_ro/current/log/unicorn.log'
 
 # Unicorn socket
 listen '/tmp/unicorn.[app name].sock'
