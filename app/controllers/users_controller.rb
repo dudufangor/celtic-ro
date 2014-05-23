381cc8d54cda000414ba026a7fbfd3ceeba28150
dudufangor <dudufangor@gmail.com>
2014-05-22 22:45:39 -0300

    testando

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user].permit)
+    @user = User.new(params.require(:user).permit(:userid, :email, :user_pass, :sex))
 
     if @user.save
       flash[:success] = 'Cadastro efetuado!'
       redirect_to new_user_path
     else
-      render action: 'edit'
+      render action: 'new'
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ActiveRecord::Base
 	self.table_name = 'login'
 
-	# attr_accessor :userid, :userpass, :userpass, :email, :sex
+	alias_attribute :username, :userid
+	alias_attribute :password, :user_pass
+	
+	validates :email, confirmation: true
+	validates :email_confirmation, presence: true
+	validates :password, confirmation: true
+	validates :password_confirmation, presence: true
+
+	validates :username, :email, uniqueness: true
+	validates :username, :email, :password, :sex, presence: true
+	validates :sex, inclusion: %w(M F)
 end
