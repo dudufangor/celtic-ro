883febb25c3b79526ce1304e8f702d905bb52aab
dudufangor <dudufangor@gmail.com>
2014-05-23 01:42:18 -0300

    validações

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,19 @@
 class User < ActiveRecord::Base
-	self.table_name = 'login'
+  self.table_name = 'login'
 
 
-	alias_attribute :username, :userid
-	alias_attribute :password, :user_pass
+  alias_attribute :username, :userid
+  alias_attribute :password, :user_pass
 
-	validates :email, confirmation: true
-	validates :email_confirmation, presence: true
-	validates :password, confirmation: true
-	validates :password_confirmation, presence: true
+  validates :email, confirmation: true
+  validates :password, confirmation: true
 
-	validates :username, :email, uniqueness: true
-	validates :username, :email, :password, :sex, presence: true
-	validates :sex, inclusion: %w(M F)
-	validates :username, :password, length: { minimum: 6, maximum: 12 }
-	validates :email, format: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i, allow_blank: true
-	validates :username, :password, format: /\A[a-z0-9]+[-a-z0-9]*[a-z0-9]+\z/i, allow_blank: true
+  validates :password_confirmation, :email_confirmation, 
+    :username, :email, :password, :sex, presence: true
+
+  validates :username, :email, uniqueness: true
+  validates :sex, inclusion: %w(M F)
+  validates :username, :password, length: { minimum: 6, maximum: 12 }, allow_blank: true
+  validates :email, format: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i, allow_blank: true
+  validates :username, :password, format: /\A[a-z0-9]+[-a-z0-9]*[a-z0-9]+\z/i, allow_blank: true
 end
